b2c80ec17e4eba6e249cfb7e4394b3d7ebbc3db6
muriloPereira006 <muriloedu006@gmail.com>
2024-02-29 22:36:46 -0300

    add: default styles

---
+++ frontend/src/components/GameFrame/index.tsx
@@ -0,0 +1,20 @@
+// import Card from "../Card";
+// import CardHeader from "../CardHeader";
+// import styled from 'styled-components'
+
+// export const BoxContainer = styled.div`
+//   width: 400px;
+//   height: 400px; 
+// `;
+
+export default function GameFrame() {
+  return (
+    // <BoxContainer>
+    //   <CardHeader />
+    //   <Card />
+    // </BoxContainer>
+    <>
+      <h4>GameFrame</h4>
+    </>
+  );
+}
