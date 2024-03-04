45704843c4d812d1ad42dc102baee7a94b5f6eba
muriloPereira006 <muriloedu006@gmail.com>
2024-03-04 16:54:06 -0300

    add: cardItem

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -2,19 +2,37 @@ import styled from 'styled-components'
 
 interface CardProps {
     content: number;
+    rightAnswer: boolean;
 }
 
-const CardStyled = styled.td`
-  width: 90px;
-  height: 90px;
-  background-color: red;
+const CardStyled = styled.td<{ rightAnswer: boolean }>`
+  width: 80px;
+  height: 80px;
+  background-color: ${props => props.rightAnswer ? 'green' : 'red'};
   border-radius: 5px;
+  cursor: pointer;
   box-sizing: border-box;
 
   text-align: center;
+  margin: 50px;
+  padding: 10px;
 `
-const Card: React.FC<CardProps> = ({ content }) => {
-    return <CardStyled>{content}</CardStyled>;
+
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: blue;
+  border-radius: 5px;
+  cursor: pointer;
+  box-sizing: border-box;
+`
+const Card: React.FC<CardProps> = ({ content, rightAnswer }) => {
+    return (<CardStyled rightAnswer={rightAnswer}>
+        <Content>{content}</Content>
+    </CardStyled>);
 };
   
 export default Card;
--- frontend/src/components/CardContainer/index.tsx
+++ frontend/src/components/CardContainer/index.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 const Table = styled.table`
   width: 100%;
   height: calc(100% - 100px);
+  border-spacing: 10px;
 `
 
 const teste = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
