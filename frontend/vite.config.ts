dcaeadafc3a4d96d1b11a312447e8fc2a591f452
muriloPereira006 <muriloedu006@gmail.com>
2024-03-13 21:58:30 -0300

    fix: .env in vite.config for port recive .env variable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const port:number = parseInt(process.env.VITE_PORT ? process.env.VITE_PORT : '3001');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: port
   }
 })
