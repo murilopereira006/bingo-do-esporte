d140e5713a364c75c22eea3ff0d31f0866ffd4ba
muriloPereira006 <muriloedu006@gmail.com>
2024-03-15 23:24:29 -0300

    draft: assets

--- backend/src/assets/index.ts
+++ backend/src/assets/index.ts
@@ -1,7 +1,31 @@
-import AchievemenetImage from '../core/achievemenetImage';
+import AchievemenetImage from "../core/achievemenetImage";
 
-export const Imagens:AchievemenetImage = {
-    id: 2,
-    name: 'Achievemene 2',
-    image_url: 'Achievemene 2',
+export const Sydney:AchievemenetImage = {
+    id: 44,
+    name: 'Sydney 2000',
+    image_url: "./img/Default_Sydney_Australia_flat_3D_miniature_0.png"
+}
+
+export const Athina:AchievemenetImage = {
+    id: 46,
+    name: 'Atenas 2004',
+    image_url: "./img/Default_Athens_Grece_flat_3D_miniature_3.png"
+}
+
+export const Beijing:AchievemenetImage = {
+    id: 48,
+    name: 'Beijing 2008',
+    image_url: "./img/Default_Beijin_China_flat_3D_miniature_3.png"
+}
+
+export const London:AchievemenetImage = {
+    id: 50,
+    name: 'Londres 2012',
+    image_url: "./img/Default_London_with_the_Big_Ben_UK_flat_3D_miniature_1.png"
+}
+
+export const Rio:AchievemenetImage = {
+    id: 52,
+    name: 'Rio de Janeiro 2016',
+    image_url: "./img/Default_Rio_de_Janeiro_Brazil_flat_3D_miniature_1_transparence-removebg-preview.png"
 }
