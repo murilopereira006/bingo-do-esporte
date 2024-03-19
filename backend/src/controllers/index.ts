44ffabb04e1c735f839e9ff88aacdc159d932df1
muriloPereira006 <muriloedu006@gmail.com>
2024-03-18 22:29:07 -0300

    draft: getting images - image_compse in controller

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -7,11 +7,7 @@ export const sortedAchievements = (): Achievement[] => {
     let responseArray: Achievement[] = []
 
     while (responseArray.length < 50) {
-        let achievement = randomAchievement(easy)
-        achievement.image_composition.map(img => {
-            img.image_url = `${path.join(__dirname, 'src', 'assets', 'img')}`
-        })
-        responseArray.push(achievement)
+        responseArray.push(randomAchievement(easy))
     }
     // sortear um ano ou cidade
     // Consferir se ano ou cidade eh existente
